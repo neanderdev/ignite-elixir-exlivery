5b42f8c513db08b441d08b7d44ad883b8cc26ff6
neanderdev <neander.faria@hotmail.com>
2022-08-30 08:41:15 -0300

    Testando a struct de Item

---
+++ test/orders/item_test.exs
@@ -0,0 +1,41 @@
+defmodule ExliveryTest do
+  use ExUnit.Case
+
+  alias Exlivery.Orders.Item
+
+  import Exlivery.Factory
+
+  describe "build/4" do
+    test "when all params are valid, returns an item" do
+      response = Item.build("Pizza de frango", :pizza, "25.5", 1)
+
+      expected_response = {:ok, build(:item)}
+
+      assert response == expected_response
+    end
+
+    test "where there is an invalid category, returns an error" do
+      response = Item.build("Pizza de frango", :teste, "25.5", 1)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+
+    test "where there is an invalid price, returns an error" do
+      response = Item.build("Pizza de frango", :pizza, "price_invalid", 1)
+
+      expected_response = {:error, "Invalid price"}
+
+      assert response == expected_response
+    end
+
+    test "where there is an invalid quantity, returns an error" do
+      response = Item.build("Pizza de frango", :pizza, "25.5", 0)
+
+      expected_response = {:error, "Invalid parameters"}
+
+      assert response == expected_response
+    end
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,7 @@ defmodule Exlivery.Factory do
   use ExMachina
 
   alias Exlivery.Users.User
+  alias Exlivery.Orders.Item
 
   def user_factory do
     %User{
@@ -10,6 +11,15 @@ defmodule Exlivery.Factory do
       address: "Av Teste, 17 - Centro",
       cpf: "123456789",
       age: 19
+    }
+  end
+
+  def item_factory do
+    %Item{
+      description: "Pizza de frango",
+      category: :pizza,
+      unity_price: Decimal.new("25.5"),
+      quantity: 1
     }
   end
 end
