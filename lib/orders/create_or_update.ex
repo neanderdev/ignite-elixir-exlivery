940cdfa5f7a9ca990ed7180924868047e83b4ccb
neanderdev <neander.faria@hotmail.com>
2022-08-30 10:00:36 -0300

    Facilitando a criação de Orders pt 1

---
+++ lib/orders/create_or_update.ex
@@ -0,0 +1,14 @@
+defmodule Exlivery.Orders.CreateOrUpdate do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Orders.Agent, as: OrderAgent
+
+  def call(%{user_cpf: user_cpf, items: items}) do
+    with {:ok, user} <- UserAgent.get(user_cpf),
+         {:ok, items} <- build_items(items),
+         {:ok, order} <- Order.build(user, items) do
+      OrderAgent.save(order)
+    else
+      error -> error
+    end
+  end
+end
